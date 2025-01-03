f550eddb055ea1e1332bbbc0074c0ac642c85f54
Yegor Bugayenko <yegor256@gmail.com>
2025-01-03 12:31:01 +0300

    year up to 2025

--- lib/netbout/message.rb
+++ lib/netbout/message.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 #
-# Copyright (c) 2024 Yegor Bugayenko
+# Copyright (c) 2024-2025 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -23,7 +23,7 @@
 
 # Message.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2024 Yegor Bugayenko
+# Copyright:: Copyright (c) 2024-2025 Yegor Bugayenko
 # License:: MIT
 class Netbout::Message
   def initialize(iri, token, json)
